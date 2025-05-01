3422980fa50a0010b8d3e68948b10f40ffc9a9dc
Juan Forero <133827052+jsForeroBiomed@users.noreply.github.com>
2025-05-01 16:02:21 -0500

    Se actualiza la función de get_connection() agregando el port

--- src/api/database.py
+++ src/api/database.py
@@ -8,6 +8,7 @@ load_dotenv()  # Carga el archivo .env automáticamente
 def get_connection():
     return mysql.connector.connect(
         host=os.getenv("DB_HOST"),
+        port=int(os.getenv("DB_PORT")),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
         database=os.getenv("DB_NAME")
