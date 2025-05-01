076de7bb1cd624911da13b9cb1216e4850211c00
Juan Forero <133827052+jsForeroBiomed@users.noreply.github.com>
2025-05-01 12:29:01 -0500

    Se comenta el código y se deja un endpoint de testeo sencillo para pruebas

--- src/api/main.py
+++ src/api/main.py
@@ -10,72 +10,76 @@ from src.api.database import get_connection
 
 app = FastAPI()
 
-class DatosEntrada(BaseModel):
-    presion_sistolica: float
-    colesterol_total: float
-    indice_masa_corporal: float
-    edad: float
-    actividad_fisica: float
-    cigarrillos_por_dia: float
+@app.get("/")
+def test():
+    return {"message": "Funciona en Railway"}
 
-
-@app.get("/check-db")
-def check_db():
-    conn = get_connection()
-    if conn:
-        conn.close()
-        return {"status": "success", "message": "Connected to the database successfully."}
-    return {"status": "error", "message": "Database connection failed."}
-
-
-@app.post("/predict")
-def predict(data: DatosEntrada) -> dict:
-    entrada = np.array([[
-
-        data.presion_sistolica,
-        data.colesterol_total,
-        data.indice_masa_corporal,
-        data.edad,
-        data.actividad_fisica,
-        data.cigarrillos_por_dia
-
-        ]])
-
-    modelo = joblib.load("modelo_tree_tuneado.pkl")
-    resultado = modelo.predict(entrada)[0]
-    
-    conn = get_connection()
-    cursor = conn.cursor()
-
-
-    query = """
-        INSERT INTO predicciones (
-            presion_sistolica,
-            colesterol_total,
-            indice_masa_corporal,
-            edad,
-            actividad_fisica,
-            cigarrillos_por_dia,
-            riesgo
-        ) VALUES (%s, %s, %s, %s, %s, %s, %s)
-    """
-
-    values = (
-        data.presion_sistolica,
-        data.colesterol_total,
-        data.indice_masa_corporal,
-        data.edad,
-        data.actividad_fisica,
-        data.cigarrillos_por_dia,
-        resultado
-    )
-
-    cursor.execute(query, values)
-    conn.commit()
-    cursor.close()
-    conn.close()
-
-    return {
-        "input": data.dict(),
-        "riesgo_cardiovascular": resultado
-        }
+# class DatosEntrada(BaseModel):
+#     presion_sistolica: float
+#     colesterol_total: float
+#     indice_masa_corporal: float
+#     edad: float
+#     actividad_fisica: float
+#     cigarrillos_por_dia: float
+# 
+# 
+# @app.get("/check-db")
+# def check_db():
+#     conn = get_connection()
+#     if conn:
+#         conn.close()
+#         return {"status": "success", "message": "Connected to the database successfully."}
+#     return {"status": "error", "message": "Database connection failed."}
+# 
+# 
+# @app.post("/predict")
+# def predict(data: DatosEntrada) -> dict:
+#     entrada = np.array([[
+# 
+#         data.presion_sistolica,
+#         data.colesterol_total,
+#         data.indice_masa_corporal,
+#         data.edad,
+#         data.actividad_fisica,
+#         data.cigarrillos_por_dia
+# 
+#         ]])
+# 
+#     modelo = joblib.load("modelo_tree_tuneado.pkl")
+#     resultado = modelo.predict(entrada)[0]
+#     
+#     conn = get_connection()
+#     cursor = conn.cursor()
+# 
+# 
+#     query = """
+#         INSERT INTO predicciones (
+#             presion_sistolica,
+#             colesterol_total,
+#             indice_masa_corporal,
+#             edad,
+#             actividad_fisica,
+#             cigarrillos_por_dia,
+#             riesgo
+#         ) VALUES (%s, %s, %s, %s, %s, %s, %s)
+#     """
+# 
+#     values = (
+#         data.presion_sistolica,
+#         data.colesterol_total,
+#         data.indice_masa_corporal,
+#         data.edad,
+#         data.actividad_fisica,
+#         data.cigarrillos_por_dia,
+#         resultado
+#     )
+# 
+#     cursor.execute(query, values)
+#     conn.commit()
+#     cursor.close()
+#     conn.close()
+# 
+#     return {
+#         "input": data.dict(),
+#         "riesgo_cardiovascular": resultado
+#         }
