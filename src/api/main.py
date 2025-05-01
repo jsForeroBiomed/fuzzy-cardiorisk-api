ba9d8f2485b3f609caf01323735a987795c01b5b
Juan Forero <133827052+jsForeroBiomed@users.noreply.github.com>
2025-05-01 15:44:25 -0500

    Se agregan control de errores en el endpoint de check-db y se comenta el endpoint de predict para hacer testing

--- src/api/main.py
+++ src/api/main.py
@@ -21,61 +21,64 @@ class DatosEntrada(BaseModel):
 
 @app.get("/check-db")
 def check_db():
-    conn = get_connection()
-    if conn:
-        conn.close()
-        return {"status": "success", "message": "Connected to the database successfully."}
-    return {"status": "error", "message": "Database connection failed."}
+    try:
+        conn = get_connection()
+        if conn:
+            conn.close()
+            return {"status": "success", "message": "Connected to the database successfully."}
+    except Exception as e:
+        return {"status": "error", "message": f"Exception: {str(e)}"}
 
 
 @app.post("/predict")
 def predict(data: DatosEntrada) -> dict:
-    entrada = np.array([[
-
-        data.presion_sistolica,
-        data.colesterol_total,
-        data.indice_masa_corporal,
-        data.edad,
-        data.actividad_fisica,
-        data.cigarrillos_por_dia
-
-        ]])
-
-    modelo = joblib.load("modelo_tree_tuneado.pkl")
-    resultado = modelo.predict(entrada)[0]
-    
-    conn = get_connection()
-    cursor = conn.cursor()
-
-
-    query = """
-        INSERT INTO predicciones (
-            presion_sistolica,
-            colesterol_total,
-            indice_masa_corporal,
-            edad,
-            actividad_fisica,
-            cigarrillos_por_dia,
-            riesgo
-        ) VALUES (%s, %s, %s, %s, %s, %s, %s)
-    """
-
-    values = (
-        data.presion_sistolica,
-        data.colesterol_total,
-        data.indice_masa_corporal,
-        data.edad,
-        data.actividad_fisica,
-        data.cigarrillos_por_dia,
-        resultado
-    )
-
-    cursor.execute(query, values)
-    conn.commit()
-    cursor.close()
-    conn.close()
-
-    return {
-        "input": data.dict(),
-        "riesgo_cardiovascular": resultado
-        }
+    return {"message": "Funciona el endpoint POST"}
+#     entrada = np.array([[
+# 
+#         data.presion_sistolica,
+#         data.colesterol_total,
+#         data.indice_masa_corporal,
+#         data.edad,
+#         data.actividad_fisica,
+#         data.cigarrillos_por_dia
+# 
+#         ]])
+# 
+#     modelo = joblib.load("modelo_tree_tuneado.pkl")
+#     resultado = modelo.predict(entrada)[0]
+#     
+#     conn = get_connection()
+#     cursor = conn.cursor()
+# 
+# 
+#     query = """
+#         INSERT INTO predicciones (
+#             presion_sistolica,
+#             colesterol_total,
+#             indice_masa_corporal,
+#             edad,
+#             actividad_fisica,
+#             cigarrillos_por_dia,
+#             riesgo
+#         ) VALUES (%s, %s, %s, %s, %s, %s, %s)
+#     """
+# 
+#     values = (
+#         data.presion_sistolica,
+#         data.colesterol_total,
+#         data.indice_masa_corporal,
+#         data.edad,
+#         data.actividad_fisica,
+#         data.cigarrillos_por_dia,
+#         resultado
+#     )
+# 
+#     cursor.execute(query, values)
+#     conn.commit()
+#     cursor.close()
+#     conn.close()
+# 
+#     return {
+#         "input": data.dict(),
+#         "riesgo_cardiovascular": resultado
+#         }
